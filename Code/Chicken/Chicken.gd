a0240bcb496b91074a4e6a8d9843b4d5ed01f768
Wyatt-Hart <wyatt.c.hart@gmail.com>
2026-05-29 02:51:44 -0700

    added UI and logic for lives counter

--- Code/Chicken/Chicken.gd
+++ Code/Chicken/Chicken.gd
@@ -2,12 +2,17 @@ extends Area3D
 class_name Chicken
 
 @onready var mesh: Node3D = $Mesh
+@onready var lives_ui: Label = $UI/LivesUI
+@onready var gameover_ui: PanelContainer = $GameOverContainer
 
+var lives: int = 8
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	area_entered.connect(on_collision)
+	lives_ui.text = "Lives: " + str(lives)
+	gameover_ui.visible = false
 
 func on_collision(area: Area3D):
 	if area is Goal:
@@ -15,9 +20,23 @@ func on_collision(area: Area3D):
 
 	if area is Car:
 		print("Dead :(")
+		killed()
+
+func killed():
+	if lives <= 0:
+		print("GAME OVER!")
+		lives_ui.visible = false
+		gameover_ui.visible = true
+		pass
+	position = Vector3.ZERO
+	mesh.rotation_degrees.y = 0
+	lives -= 1
+	lives_ui.text = "Lives: " + str(lives)
 
 
 func _process(delta: float) -> void:
+	if lives < 0:
+		return
 	if Input.is_action_just_pressed("move_left"):
 		position.x += 1
 		mesh.rotation_degrees.y = 90.0;
